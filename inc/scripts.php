a83d8da7a7a81b1dd83294d1773f23203bef6d6a
Aminul Islam <aminmedia@gmail.com>
2014-12-20 23:06:51 +0600

    Google Font added

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,12 +1,62 @@
 <?php
 
+if ( ! function_exists( '_sbs_fonts_url' ) ) :
+/**
+ * Register Google fonts for _sBS
+ *
+ * @since 1.0.1
+ *
+ * @return string Google fonts URL for the theme.
+ */
+function _sbs_fonts_url() {
+	$fonts_url = '';
+	$fonts     = array();
+	$subsets   = 'latin,latin-ext';
+
+	/* translators: If there are characters in your language that are not supported by Raleway, translate this to 'off'. Do not translate into your own language. */
+	if ( 'off' !== _x( 'on', 'Raleway font: on or off', PROJECT_TEXT_DOMAIN ) ) {
+		$fonts[] = 'Raleway:500,600,700,400,300';
+	}
+
+	/* translators: If there are characters in your language that are not supported by Roboto, translate this to 'off'. Do not translate into your own language. */
+	if ( 'off' !== _x( 'on', 'Roboto font: on or off', PROJECT_TEXT_DOMAIN ) ) {
+		$fonts[] = 'Roboto:400,300,300italic,400italic,500,500italic,700,700italic';
+	}
+
+	/* translators: To add an additional character subset specific to your language, translate this to 'greek', 'cyrillic', 'devanagari' or 'vietnamese'. Do not translate into your own language. */
+	$subset = _x( 'no-subset', 'Add new subset (greek, cyrillic, devanagari, vietnamese)', PROJECT_TEXT_DOMAIN );
+
+	if ( 'cyrillic' == $subset ) {
+		$subsets .= ',cyrillic,cyrillic-ext';
+	} elseif ( 'greek' == $subset ) {
+		$subsets .= ',greek,greek-ext';
+	} elseif ( 'devanagari' == $subset ) {
+		$subsets .= ',devanagari';
+	} elseif ( 'vietnamese' == $subset ) {
+		$subsets .= ',vietnamese';
+	}
+
+	if ( $fonts ) {
+		$fonts_url = add_query_arg( array(
+			'family' => urlencode( implode( '|', $fonts ) ),
+			'subset' => urlencode( $subsets ),
+		), '//fonts.googleapis.com/css' );
+	}
+
+	return $fonts_url;
+}
+endif;
+
+
+
 /**
  * Enqueue scripts and styles.
- * 
- * @package _sBS
+ *
  * @since 1.0.0
  */
 function _sbs_scripts() {
+	// Add google fonts, used in the main stylesheet.
+	wp_enqueue_style( '_sbs-google-fonts', _sbs_fonts_url(), array(), null );
 	// Bootstrap style
 	wp_enqueue_style( '_sbs-bootstrap', PROJECT_ASSETS . '/css/bootstrap.min.css', array(), PROJECT_BS_VERSION, 'all');
 	// Font Awesome style
