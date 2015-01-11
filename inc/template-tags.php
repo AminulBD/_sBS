244c567545abfca7a73de47b9d942abaf1d399b4
Aminul Islam <aminmedia@gmail.com>
2015-01-11 23:57:53 +0600

    Added wp-core page navigation

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -22,11 +22,11 @@ function the_posts_navigation() {
 		<ul class="pager">
 
 			<?php if ( get_next_posts_link() ) : ?>
-			<li class="previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', _SBS_TEXT_DOMAIN ) ); ?></li>
+			<li class="previous"><?php next_posts_link( __( 'Older posts', _SBS_TEXT_DOMAIN ) ); ?></li>
 			<?php endif; ?>
 
 			<?php if ( get_previous_posts_link() ) : ?>
-			<li class="next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', _SBS_TEXT_DOMAIN ) ); ?></li>
+			<li class="next"><?php previous_posts_link( __( 'Newer posts', _SBS_TEXT_DOMAIN ) ); ?></li>
 			<?php endif; ?>
 
 		</ul><!-- .nav-links -->
